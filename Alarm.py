26b5315b6a153f5ca185e0ee4c61c15dc8799872
rlong2 <53006195+rlong2@users.noreply.github.com>
2020-11-12 19:06:13 -0700

    Update Alarm.py
    Added comment that Alarms still needs to write to a file

--- Alarm.py
+++ Alarm.py
@@ -1,6 +1,7 @@
 "Create Alarm Class here for the CS3080 python final project."
 
 # This is a PLACEHOLDER Alarm class to test initial functionality of the ReadAlarms() method
+# This class still needs to write to a file
 
 class Alarm:
     def __init__(self, name="default_name", time="12:00", description="default_description", email="default@email.com"):
