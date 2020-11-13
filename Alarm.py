ba275bb6076fe98f506e44b50c79c55d5ae77717
rlong2 <rlong2@protonmail.com>
2020-11-12 18:37:53 -0700

    Update Alarm.py
    This is a placeholder so ReadAlarms can use the Alarms() method. This class still needs to write to a file

--- Alarm.py
+++ Alarm.py
@@ -1,1 +1,13 @@
 "Create Alarm Class here for the CS3080 python final project."
+
+# This is a PLACEHOLDER Alarm class to test initial functionality of the ReadAlarms() method
+
+class Alarm:
+    def __init__(self, name="default_name", time="12:00", description="default_description", email="default@email.com"):
+                 self.name = name
+                 self.time = time
+                 self.description = description
+                 self.email = email
+    def __str__(self):
+        return "Name: " + self.name + "\nTime due: " + self.time + "\nDescription: " + self.description + "\nEmail: " + self.email
+
